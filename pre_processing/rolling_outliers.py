ad62028267448ca866d1ae2cbe73307a379c098d
Bruno Alves <bruno.afonso.alves@tecnico.ulisboa.pt>
2019-03-24 18:32:35 +1100

    repo structuring

---
+++ pre_processing/rolling_outliers.py
@@ -0,0 +1,47 @@
+import glob
+import numpy as np
+from astropy.io import fits
+import matplotlib.pyplot as plt
+
+def get_spectra(filename):
+    spectra = glob.glob(filename+'*.fits')    
+    spectra_number = len(spectra)
+
+    with fits.open(spectra[0]) as hdu:
+        f = hdu[1].data['flux']
+        f = f[0:3750]
+        f = f[250:]
+        f = f.astype(np.float32)
+
+    plt.plot(np.arange(0,3500), f)
+    plt.savefig('test.png')
+    plt.close()
+    return f
+
+spec = get_spectra('/fred/oz012/Bruno/data/spectra/spec-3699-55517-0981')
+print("hhh")
+
+def outlier_id_roll(arr, cut=2.5):
+    """Removs of noisy peaks """
+    l = len(arr)
+    width = int(l / 10)
+    id_flags = np.zeros(l)
+    for m1 in range(2*width,l-3*width):
+        m2 = m1 + width
+        """
+        dist = np.abs(arr[m1:m2] - np.median(arr[m1:m2]))
+        medmed = np.median(dist)
+        s = dist / (medmed if medmed else 1.)
+        """
+        dist = np.abs(arr[m1:m2] - np.mean(arr[m1:m2]))
+        s = dist / np.std(arr[m1:m2])
+        id_flags[m1:m2] = s > cut
+    return id_flags
+
+roll_ids = outlier_id_roll(spec)
+spec[roll_ids == 1] = 0.
+
+plt.plot(np.arange(0,3500), spec)
+plt.ylim([-16,16])
+plt.savefig('test2.png')
+plt.close()
--- writer.py
+++ writer.py
@@ -1,27 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #from src.data.spectra_generation import gen_spectrum_2lines as gen
-from src.data.tfrecords import real_spectra_to_tfrecord
+#from src.data.tfrecords import real_spectra_to_tfrecord
 #from src.data.tfrecords import pictures_to_tfrecord
 #from src.data.tfrecords import gen_spectra_to_tfrecord
+from src.data import fits as ff
 
-def plot(x, y, s):
-    plt.plot(x, y)
-    plt.show()
-    #plt.savefig(s)
-    plt.close()
-
-#pictures_to_tfrecord(filename='/fred/oz012/Bruno/data/celebA/celebA', 
-#                     data_folder='/fred/oz012/Bruno/data/celebA/', nshards=100)
-real_spectra_to_tfrecord(filename='/fred/oz012/Bruno/data/spectra/legacy/legacy_bit6/spectra_grid', 
-                         table_path='/fred/oz012/Bruno/data/Legacy6ListAllValues.csv', 
-                         nshards=100, write_fits=True)
-#gen_spectra_to_tfrecord(100000, 3500, '/fred/oz012/Bruno/data/spectra/legacy/outliers/spectra',100,norm=True)
-
+ff.to_fits(np.arange(3500),np.ones((3500)),"x", "y", "test.fits")
 """
-classes= [3]
-x, y, _ = gen(len(classes), 3500, classes, norm=True)
-for i in range(len(y)):
-    plot(x[i],y[i], str(i)+'.png')
+pictures_to_tfrecord(filename='/fred/oz012/Bruno/data/celebA/celebA', 
+                     data_folder='/fred/oz012/Bruno/data/celebA/', nshards=100)
+real_spectra_to_tfrecord(filename='/fred/oz012/Bruno/data/spectra/gal_form_burst/spectra', 
+                         table_path='/fred/oz012/Bruno/data/GAL_STARFORMING_STARBURSTListAllValues.csv', 
+                         nshards=100, write_fits=False)
+gen_spectra_to_tfrecord(100000, 3500, '/fred/oz012/Bruno/data/spectra/legacy/outliers/spectra',100,norm=True)
 """
+
+
 
