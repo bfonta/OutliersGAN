899738862cbbc254bba1838457cd8db753bb4a54
Bruno Alves <bruno.afonso.alves@tecnico.ulisboa.pt>
2019-03-25 11:26:09 +1100

    removed useless package

--- writer.py
+++ writer.py
@@ -1,18 +1,20 @@
-import matplotlib.pyplot as plt
 import numpy as np
+
 #from src.data.spectra_generation import gen_spectrum_2lines as gen
-#from src.data.tfrecords import real_spectra_to_tfrecord
+from src.data.tfrecords import real_spectra_to_tfrecord
 #from src.data.tfrecords import pictures_to_tfrecord
 #from src.data.tfrecords import gen_spectra_to_tfrecord
-from src.data import fits as ff
+#from src.data import fits as ff
 
-ff.to_fits(np.arange(3500),np.ones((3500)),"x", "y", "test.fits")
 """
+ff.to_fits(np.arange(3500),np.ones((3500)),"x", "y", "test.fits")
 pictures_to_tfrecord(filename='/fred/oz012/Bruno/data/celebA/celebA', 
                      data_folder='/fred/oz012/Bruno/data/celebA/', nshards=100)
-real_spectra_to_tfrecord(filename='/fred/oz012/Bruno/data/spectra/gal_form_burst/spectra', 
-                         table_path='/fred/oz012/Bruno/data/GAL_STARFORMING_STARBURSTListAllValues.csv', 
+"""
+real_spectra_to_tfrecord(filename='/fred/oz012/Bruno/data/spectra/all_qso/spectra', 
+                         table_path='/fred/oz012/Bruno/data/QSOListAllValues.csv', 
                          nshards=100, write_fits=False)
+"""
 gen_spectra_to_tfrecord(100000, 3500, '/fred/oz012/Bruno/data/spectra/legacy/outliers/spectra',100,norm=True)
 """
 
