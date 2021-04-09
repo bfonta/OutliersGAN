2e8f07bc94d5f8e998ce5be31814883ef082201e
Bruno Alves <balves16@hotmail.com>
2021-04-09 19:07:53 +0200

    interactive plot changed structure

--- plots/SpectraGAN.py
+++ plots/SpectraGAN.py
@@ -11,6 +11,7 @@ from bokeh.models import Title
 from bokeh.models import Plot
 from bokeh.models import Slider
 from bokeh.models import Circle
+from bokeh.models import Legend
 from bokeh.models import Range1d
 from bokeh.models import CustomJS
 from bokeh.models import HoverTool
@@ -18,18 +19,30 @@ from bokeh.models import LinearAxis
 from bokeh.models import ColumnDataSource
 from bokeh.models import SingleIntervalTicker
 
-from bokeh.palettes import Spectral6
+from bokeh.palettes import Set3
 from bokeh.plotting import figure, show
 from bokeh.layouts import layout
 
-FILENAME = os.path.basename(__file__) + '.hdf5'
-WIDTH, HEIGHT = 2000, 400
+FILENAME = os.path.basename(__file__).replace('.py','.hdf5')
+RENDERERS = 'source1_init', 'source2_init'
+TOTALWIDTH, WIDTHRATIO = 2500, 0.7
+WIDTH, HEIGHT = int(TOTALWIDTH*WIDTHRATIO), 550
+WIDTH2, HEIGHT2 = int(TOTALWIDTH*(1-WIDTHRATIO)), HEIGHT/2
 NPOINTS = 3500
-NEPOCHS = 3
+NEPOCHS = 5
 NSTATS = 2000
 NBINS = 100
+NSPECTRA = 5
+NDIGITS = int(np.floor(np.log10(np.abs(NEPOCHS))) + 1) #number of digits in a integer
+YSHIFT = 0.3
 
-def add_axis(fig, xlabel, ylabel, xinterval=500, yinterval=.25, xbounds=None, ybounds=None):
+def add_legend(fig, obj, label, **kwargs):
+    leg = Legend(
+        items=[(label, [obj])],
+        **kwargs)
+    fig.add_layout(leg, 'center')
+    
+def add_axis(fig, xlabel=None, ylabel=None, xinterval=500, yinterval=None, xbounds=None, ybounds=None):
     color = '#000066'
     AXIS_FORMATS = dict(
         minor_tick_in=None,
@@ -48,27 +61,30 @@ def add_axis(fig, xlabel, ylabel, xinterval=500, yinterval=.25, xbounds=None, yb
         major_tick_line_width=1.5,
     )
 
-    xaxis = LinearAxis(
-        ticker     = SingleIntervalTicker(interval=xinterval),
-        axis_label = xlabel,
-        **AXIS_FORMATS
-    )
-    if xbounds:
-        xaxis.bounds = xbounds
+    if xlabel:
+        xaxis = LinearAxis(
+            ticker     = SingleIntervalTicker(interval=xinterval),
+            axis_label = xlabel,
+            **AXIS_FORMATS
+        )
+        if xbounds:
+            xaxis.bounds = xbounds
+        fig.add_layout(xaxis, 'below')
 
-    yaxis = LinearAxis(
-        ticker     = SingleIntervalTicker(interval=yinterval),
-        axis_label = ylabel,
-        **AXIS_FORMATS
-    )
-    if ybounds:
-        yaxis.bounds = ybounds
+    if ylabel:
+        yaxis = LinearAxis(
+            ticker     = SingleIntervalTicker(interval=yinterval),
+            axis_label = ylabel,
+            **AXIS_FORMATS
+        )
+        if ybounds:
+            yaxis.bounds = ybounds
+        fig.add_layout(yaxis, 'left')
 
-    fig.add_layout(xaxis, 'below')
-    fig.add_layout(yaxis, 'left')
-
-def create_figure(w, h, title, xlims=None, ylims=None):
-    p = figure(plot_width=w, plot_height=h)
+def create_figure(w, h, title, xlims=None, ylims=None, xaxislog=False):
+    p = figure(plot_width=w, plot_height=h, x_axis_type='log' if xaxislog else 'linear')
+    p.toolbar.logo = None
+    #p.toolbar_location = None
     p.axis.visible = False
     p.title = Title(text=title, text_font_size='12pt')
     if ylims:
@@ -82,69 +98,107 @@ def add_graph(fig, xname, yname, renderer_source, color):
     fig.line(x=xname, y=yname, color=color, source=renderer_source)
 
 def add_bars(fig, xname, yname, width, renderer_source, color):
-    fig.vbar(x=xname, top=yname, bottom=0, width=width, fill_color=color, source=renderer_source)
+    fig.vbar(x=xname, top=yname, bottom=0, width=width, fill_color=color, line_color='black', source=renderer_source)
 
 def add_vline(fig, xname, yname, width, renderer_source, color):
-    fig.line(x=xname, y=yname, line_width=width, color=color, source=renderer_source)
+    return fig.line(x=xname, y=yname, line_width=width, color=color, source=renderer_source)
 
-def get_normal_data():
-    x = np.arange(0,NPOINTS)
-    y = np.random.normal(loc=0, scale=.2, size=(NEPOCHS,NPOINTS))
-    return x, y
-
-def get_stats_data():
-    x = np.random.normal(loc=1, scale=.2, size=(NEPOCHS,NSTATS))
-    y = np.random.normal(loc=1, scale=.2, size=(NEPOCHS,NSTATS))
-    return x, y
-
-with h5py.File(FILENAME, 'w') as f1:
-    arr_x, arr_y = get_normal_data()
-    arr_mean, arr_std = get_stats_data()
-    dset1 = f1.create_dataset("wavelength", (1,NPOINTS), dtype='f', data=arr_x)
-    dset2 = f1.create_dataset("flux", (NEPOCHS,NPOINTS), dtype='f', data=arr_y)
-    dset3 = f1.create_dataset("means", (NEPOCHS,NSTATS), dtype='f', data=arr_mean)
-    dset4 = f1.create_dataset("stds", (NEPOCHS,NSTATS), dtype='f', data=arr_std)
-
-with h5py.File(FILENAME, 'r') as f2:
-    #print(list(f2.keys()))
-    arr_x =    f2['wavelength'][0,:]
-    arr_y =    f2['flux'][:]
-    arr_mean = f2['means'][:]
-    arr_std =  f2['stds'][:]
-
+with h5py.File(FILENAME, 'r') as f1:
+    y, means, stds = ([] for _ in range(3))
+    npoints = len(f1['flux']['flux_{}'.format('0'.zfill(NDIGITS))][:])
+    w_i = f1['meta']['wavelength'][0]
+    w_d = f1['meta']['wavelength'][1]
+    arr_x = np.arange(w_i, w_i+w_d*npoints, w_d)
+    for e in range(NEPOCHS):
+        estr = str(e).zfill(NDIGITS)
+        y.append( np.expand_dims(f1['fluxsamp']['flux_samp_'+estr][:NSPECTRA,:], axis=0) )
+        means.append( f1['meanssamp']['means_samp_'+estr][:] )
+        stds.append( f1['stdssamp']['stds_samp_'+estr][:] )
+    yreal = f1['flux']['flux_{}'.format('0'.zfill(NDIGITS))][:]
+    meansreal = f1['means']['means_{}'.format('0'.zfill(NDIGITS))][:]
+    stdsreal = f1['stds']['stds_{}'.format('0'.zfill(NDIGITS))][:]
+arr_y = np.vstack(tuple(x for x in y))
+#print(arr_y.shape) NEPOCHS*NSPECTRA
+arr_mean = np.vstack(tuple(x for x in means))
+arr_std = np.vstack(tuple(x for x in stds))
+    
 epochs = [x for x in range(NEPOCHS)]
 s1, s2 = dict(), dict()
 xs1, ys1 = 'wavelength', 'flux'
 xs2, ys2 = ('means', 'stds'), ('meanscounts', 'stdscounts')
 xse, yse = ('mean1x', 'mean2x'), ('mean1y', 'mean2y')
+wstr = 'width1', 'width2'
 
-hstdmax, hmeanmax = -999, -999
-hstdmin, hmeanmin = 999, 999
+def handle_histogram(arr):
+    h, edg = np.histogram(arr, NBINS)
+    c = (edg[:-1] + edg[1:])/2
+    return h, c
+    
+hstdmax, hmeanmax, cmeanmax, cstdmax = (-999 for _ in range(4))
+hstdmin, hmeanmin, cmeanmin, cstdmin = (999 for _ in range(4))
+hstdmaxreal, hmeanmaxreal, cmeanmaxreal, cstdmaxreal = (-999 for _ in range(4))
+hstdminreal, hmeanminreal, cmeanminreal, cstdminreal = (999 for _ in range(4))
 for epoch in range(NEPOCHS):
     s1name, s2name = 'source1_{}'.format(epoch), 'source2_{}'.format(epoch)
-    s1[s1name] = ColumnDataSource(data={xs1: arr_x.tolist(),
-                                        ys1: arr_y[epoch].tolist()})
+    s1[s1name] = ColumnDataSource(data={xs1: arr_x.tolist()})
+    for i in range(NSPECTRA):
+        s1[s1name].data[ys1+str(i)] = (arr_y[epoch][i]+i*YSHIFT).tolist()
 
-    hmean, edgmean = np.histogram(arr_mean[epoch], NBINS)
-    cmean = (edgmean[:-1] + edgmean[1:])/2
-    hstd, edgstd = np.histogram(arr_std[epoch], NBINS)
-    cstd = (edgstd[:-1] + edgstd[1:])/2
+    hmean, cmean = handle_histogram(arr_mean[epoch])
+    hstd, cstd = handle_histogram(arr_std[epoch])
     s2[s2name] = ColumnDataSource(data={xs2[0]: cmean.tolist(),
                                         ys2[0]: hmean.tolist(),
                                         xs2[1]: cstd.tolist(),
-                                        ys2[1]: hstd.tolist()})
-    if np.max(hstd) > hstdmax:
-        hstdmax = np.max(hstd)
-    if np.min(hstd) < hstdmin:
-        hstdmin = np.min(hstd)
-    if np.max(hmean) > hmeanmax:
-        hmeanmax = np.max(hmean)
-    if np.min(hmean) < hmeanmin:
-        hmeanmin = np.min(hmean)
+                                        ys2[1]: hstd.tolist(),
+                                        wstr[0]: [cmean[1]-cmean[0] for _ in range(len(cmean))],
+                                        wstr[1]: [cstd[1]-cstd[0] for _ in range(len(cstd))]})
+
+    if epoch==0:
+        initmeanx, initstdx = np.mean(cmean), np.mean(cstd)
+        s1[RENDERERS[0]] = ColumnDataSource(data={xs1: arr_x.tolist()})
+        for i in range(NSPECTRA):
+            s1[RENDERERS[0]].data[ys1+str(i)] = (arr_y[epoch][i]+i*YSHIFT).tolist()
+        s2[RENDERERS[1]] = ColumnDataSource(data={xs2[0]: cmean.tolist(),
+                                                  ys2[0]: hmean.tolist(),
+                                                  xs2[1]: cstd.tolist(),
+                                                  ys2[1]: hstd.tolist(),
+                                                  wstr[0]: [cmean[1]-cmean[0] for _ in range(len(cmean))],
+                                                  wstr[1]: [cstd[1]-cstd[0] for _ in range(len(cstd))]})
+
+
+    def get_min_max(data, dmax, dmin):
+        if np.max(data) > dmax:
+            dmax = np.max(data)
+        if np.min(data) < dmin:
+            dmin = np.min(data)
+        return dmax, dmin
+
+    hmeanmax, hmeanmin = get_min_max(hmean, hstdmax, hstdmin)
+    hstdmax, hstdmin = get_min_max(hstd, hstdmax, hstdmin)
+    cmeanmax, cmeanmin = get_min_max(cmean, cmeanmax, cmeanmin)
+    cstdmax, cstdmin = get_min_max(cstd, cstdmax, cstdmin)
+
+hmeanreal, cmeanreal = handle_histogram(meansreal)
+hstdreal, cstdreal = handle_histogram(stdsreal)
+hmeanmaxreal, hmeanminreal = get_min_max(hmeanreal, hstdmaxreal, hstdminreal)
+hstdmaxreal, hstdminreal = get_min_max(hstdreal, hstdmaxreal, hstdminreal)
+cmeanmaxreal, cmeanminreal = get_min_max(cmeanreal, cmeanmaxreal, cmeanminreal)
+cstdmaxreal, cstdminreal = get_min_max(cstdreal, cstdmaxreal, cstdminreal)
+sreal1 = ColumnDataSource(data={xs1: arr_x.tolist(),
+                               ys1: yreal.tolist()})
+sreal2 = ColumnDataSource(data={xs2[0]: cmeanreal.tolist(),
+                                ys2[0]: hmeanreal.tolist(),
+                                xs2[1]: cstdreal.tolist(),
+                                ys2[1]: hstdreal.tolist(),
+                                wstr[0]: [cmeanreal[1]-cmeanreal[0] for _ in range(len(cmeanreal))],
+                                wstr[1]: [cstdreal[1]-cstdreal[0] for _ in range(len(cstdreal))]})
 
 sextraname = 'sextra'
-sextra = ColumnDataSource(data={xse[0]: [1, 1], yse[0]: [hmeanmin, hmeanmax],
-                                xse[1]: [1, 1], yse[1]: [hstdmin, hstdmax]})
+sextra = ColumnDataSource(data={xse[0]: [initmeanx,initmeanx], yse[0]: [hmeanmin, hmeanmax],
+                                xse[1]: [initstdx,initstdx], yse[1]: [hstdmin, hstdmax]})
+
+sextra_real = ColumnDataSource(data={xse[0]: [initmeanx,initmeanx], yse[0]: [hmeanmin, hmeanmax],
+                                     xse[1]: [initstdx,initstdx], yse[1]: [hstdmin, hstdmax]})
 
 dict_sources_1 = dict(zip(epochs, ['source1_{}'.format(x) for x in epochs]))
 dict_sources_2 = dict(zip(epochs, ['source2_{}'.format(x) for x in epochs]))
@@ -152,44 +206,77 @@ js_sources_1 = str(dict_sources_1).replace("'", "")
 js_sources_2 = str(dict_sources_2).replace("'", "")
 js_sources_e = sextraname
 
-p1 = create_figure(WIDTH, HEIGHT, 'Generated Spectrum', ylims=[-1,1])
-add_axis(p1, 'Wavelength [' + u'\u212b' + ']', 'Flux', ybounds=(-2.01,1.01))
-add_graph(p1, xs1, ys1, s1['source1_0'], '#660044')
+spectra_ylims = [-0.15,1.5]
+spectra_yint = (spectra_ylims[0]-spectra_ylims[0])/6
+pfake = create_figure(WIDTH, HEIGHT, 'Generated QSO Spectra', ylims=spectra_ylims)
+add_axis(pfake, xlabel='Wavelength [' + u'\u212b' + ']', ylabel='Normalized Flux',
+         ybounds=spectra_ylims, xinterval=w_d*npoints/5, yinterval=spectra_yint)
+for i in range(NSPECTRA):
+    add_graph(pfake, xs1, ys1+str(i), s1[RENDERERS[0]], color=Set3[NSPECTRA][i])
 
-p_mean = create_figure(int(WIDTH/2), HEIGHT, 'Generated Means', xlims=[0.,2.], ylims=[hmeanmin,hmeanmax])
-add_axis(p_mean, 'Means [flux units]', 'Counts', xinterval=.25, yinterval=10)
-add_bars(p_mean, xs2[0], ys2[0], edgmean[1]-edgmean[0], s2['source2_0'], '#660044')
-add_vline(p_mean, xse[0], yse[0], 3., sextra, '#008000')
+preal = create_figure(WIDTH, HEIGHT, 'Real Spectra', ylims=spectra_ylims)
+add_axis(preal, xlabel='Wavelength [' + u'\u212b' + ']', ylabel='Normalized Flux',
+         ybounds=spectra_ylims, xinterval=w_d*npoints/5, yinterval=spectra_yint)
+for i in range(NSPECTRA):
+    add_graph(preal, xs1, ys1, sreal1, '#660044')
 
-p_std = create_figure(int(WIDTH/2), HEIGHT, 'Generated Standard Deviations', xlims=[0.,2.], ylims=[hstdmin,hstdmax])
-add_axis(p_std, 'Standard Deviations [flux units]', 'Counts', xinterval=.25, yinterval=10)
-add_bars(p_std, xs2[1], ys2[1], edgstd[1]-edgstd[0], s2['source2_0'], '#660044')
-add_vline(p_std, xse[1], yse[1], 3., sextra, '#008000')
+p_mean = create_figure(int(WIDTH2), int(HEIGHT2), 'Generated Means',
+                       xlims=[cmeanmin,cmeanmax], ylims=[hmeanmin,hmeanmax],
+                       xaxislog=True)
+add_axis(p_mean, xlabel=' ', ylabel='Counts',
+         xinterval=(cmeanmax-cmeanmin)/10, yinterval=hmeanmax/5)
+add_bars(p_mean, xs2[0], ys2[0], wstr[0], s2[RENDERERS[1]], '#660044')
+line = add_vline(p_mean, xse[0], yse[0], 3., sextra, '#008000')
+add_legend(fig=p_mean, obj=line, label='Mean')
+
+p_mean_real = create_figure(int(WIDTH2), int(HEIGHT2), 'Real Means',
+                            xlims=[cmeanminreal,cmeanmaxreal], ylims=[hmeanminreal,hmeanmaxreal],
+                            xaxislog=True)
+add_axis(p_mean_real, xlabel=' ', ylabel='Counts',
+         xinterval=(cmeanmaxreal-cmeanminreal)/10, yinterval=hmeanmaxreal/5)
+add_bars(p_mean_real, xs2[0], ys2[0], wstr[0], sreal2, '#660044')
+line = add_vline(p_mean_real, xse[0], yse[0], 3., sextra_real, '#008000')
+add_legend(fig=p_mean_real, obj=line, label='Mean')
+
+p_std = create_figure(int(WIDTH2), int(HEIGHT2), 'Generated Standard Deviations',
+                      xlims=[cstdmin,cstdmax], ylims=[hstdmin,hstdmax],
+                      xaxislog=True)
+add_axis(p_std, xlabel=' ', ylabel='Counts',
+         xinterval=(cstdmax-cstdmin)/10, yinterval=hstdmaxreal/5)
+add_bars(p_std, xs2[1], ys2[1], wstr[1], s2[RENDERERS[1]], '#660044')
+line = add_vline(p_std, xse[1], yse[1], 3., sextra, '#008000')
+add_legend(fig=p_std, obj=line, label='Mean')
+
+p_std_real = create_figure(int(WIDTH2), int(HEIGHT2), 'Real Standard Deviations',
+                           xlims=[cstdminreal,cstdmaxreal], ylims=[hstdminreal,hstdmaxreal],
+                           xaxislog=True)
+add_axis(p_std_real, xlabel=' ', ylabel='Counts',
+         xinterval=(cstdmaxreal-cstdminreal)/10, yinterval=hstdmaxreal/5)
+add_bars(p_std_real, xs2[1], ys2[1], wstr[1], sreal2, '#660044')
+line = add_vline(p_std_real, xse[1], yse[1], 3., sextra_real, '#008000')
+add_legend(fig=p_std_real, obj=line, label='Mean')
 
 code = """
     var epoch = slider.value;
-    var s2 = {js_sources_2};
-    var new_s2 = s2[epoch].data;
-    s2_update.data = new_s2;
-    s2_update.change.emit();
-""".format(js_sources_1=js_sources_1, js_sources_2=js_sources_2,
-           js_sources_e=js_sources_e,
-           vlinemax1=hmeanmax, vlinemax2=hstdmax)
-
-"""
     var s1 = {js_sources_1};
+    var s2 = {js_sources_2};
     var se = {js_sources_e};
+
     var new_s1 = s1[epoch].data;
     s1_update.data = new_s1;
     s1_update.change.emit();
+
+    var new_s2 = s2[epoch].data;
+    s2_update.data = new_s2;
+    s2_update.change.emit();
 
     var d = s2_update.data;
 
     var ncounts1_update = 0;
     var mean1_update = 0.;
-    for (var i = 0; i<d['means'].length; i++) {{
-        mean1_update += d['meanscounts'][i] * d['means'][i];
-        ncounts1_update += d['meanscounts'][i];
+    for (var i = 0; i<d['{means1}'].length; i++) {{
+        mean1_update += d['{meanscounts1}'][i] * d['{means1}'][i];
+        ncounts1_update += d['{meanscounts1}'][i];
     }}
     mean1_update /= ncounts1_update;
     se.data['mean1x'][0] = mean1_update;
@@ -199,9 +286,9 @@ code = """
 
     var ncounts2_update = 0;
     var mean2_update = 0.;
-    for (var i = 0; i<d['means'].length; i++) {{
-        mean2_update += d['meanscounts'][i] * d['means'][i];
-        ncounts2_update += d['meanscounts'][i];
+    for (var i = 0; i<d['{means2}'].length; i++) {{
+        mean2_update += d['{meanscounts2}'][i] * d['{means2}'][i];
+        ncounts2_update += d['{meanscounts2}'][i];
     }}
     mean2_update /= ncounts2_update;
     se.data['mean2x'][0] = mean2_update;
@@ -209,20 +296,25 @@ code = """
     se.data['mean2y'][0] = 0;
     se.data['mean2y'][1] = {vlinemax2};
     se.change.emit();
-"""
+""".format(js_sources_1=js_sources_1, js_sources_2=js_sources_2,
+           js_sources_e=js_sources_e,
+           vlinemax1=hmeanmax, vlinemax2=hstdmax,
+           wstr1=wstr[0],wstr2=wstr[1],
+           means1='means', means2='stds',
+           meanscounts1='meanscounts', meanscounts2='stdscounts')
 
-callback_args = copy.deepcopy(s1)
+callback_args = s1
 callback_args.update(s2)
 callback_args.update({sextraname: sextra})
 callback = CustomJS(args=callback_args, code=code)
-print(callback_args)
-    
-slider = Slider(start=0, end=NEPOCHS-1, value=0, step=1, title="Epoch", width=WIDTH)
-slider.js_on_change("value", callback)
 
-callback.args['s1_update'] = s1['source1_0']
-callback.args['s2_update'] = s2['source2_0'] #SOME WEIRD BUG: DOES NOT WORK WITH 'source2_1', for instance
+slider = Slider(start=0, end=NEPOCHS-1, value=0, step=1,
+                title='Epoch', width=int(WIDTH/2))
+slider.js_on_change('value', callback)
+
+callback.args['s1_update'] = s1[RENDERERS[0]]
+callback.args['s2_update'] = s2[RENDERERS[1]]
 callback.args['slider'] = slider
 
-display = layout([[p1], [p_mean, p_std], [slider]])
+display = layout([[pfake, [p_mean, p_std]], [preal, [p_mean_real, p_std_real]], [slider]])
 show(display)
